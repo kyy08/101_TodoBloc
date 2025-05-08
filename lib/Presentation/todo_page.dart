c770da18a27c3f56fc867e94abdf4564f256d136
kyy08 <kyyelzz1@gmail.com>
2025-05-08 18:09:43 +0700

    membuat halaman Todopage dengan struktru dasar

---
+++ lib/Presentation/todo_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:counter_convert_todo/bloc/todo_bloc.dart';
+import 'package:counter_convert_todo/Model/todo.dart';
+
+
+class TodoPage extends StatelessWidget {
+  const TodoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _key = GlobalKey<FormState>();
+    final _controller = TextEditingController();
+
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0), // Fixed padding
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
